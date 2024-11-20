dca4c46753087ef473ccb74c633ca06a7578d760
unknown <a.cina@procreatech.com>
2024-11-20 15:11:54 +0100

    20. Router Guards Section Introduction - Authentication Store Review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ import { AboutComponent } from "./about/about.component";
 import { CoursesModule } from "./courses/courses.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
+// un authentication router guard è un service speciale che ci consente di determinare se un utente può o non può accedere a determinate rotte
+// implementiamo questo sistema tramite Router
+
 const routes: Routes = [
   {
     path: "",
